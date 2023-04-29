2a3a44a12045d0aa8bc5b49913414255466d2dce
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2023-04-29 18:03:12 +0530

    Lesson2 Completed
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

---
+++ lesson2/src/script.js
@@ -0,0 +1,30 @@
+import './style.css'
+import * as THREE from 'three'
+
+const canvas = document.querySelector('.webGL')
+//Scene
+const scene = new THREE.Scene()
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'cyan' })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Sizes
+const sizes = {
+   width: 800,
+   height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+   canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
