3577246f9941ec0432830b2b76fcf00180956761
Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>
2023-04-29 18:02:30 +0530

    Lesson1 Completed
    Signed-off-by: Atul chourasiya <82812953+atulchourasiya@users.noreply.github.com>

---
+++ lesson1/script.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('.webGL');
+//Scene
+const scene = new THREE.Scene()
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'cyan'})
+const mesh =new THREE.Mesh(geometry,material)
+scene.add(mesh)
+
+//Sizes
+const sizes={
+   width :800,
+   height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height);
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 2
+scene.add(camera)
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+   canvas : canvas
+}) 
+renderer.setSize(sizes.width ,sizes.height)
+renderer.render(scene,camera)
